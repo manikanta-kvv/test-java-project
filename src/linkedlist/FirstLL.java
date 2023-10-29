7fe317a386e82c669dafb88405fe91e273cdef07
manikanta-kvv <106313314+manikanta-kvv@users.noreply.github.com>
2023-10-29 10:15:42 +0530

    LL package added

---
+++ src/linkedlist/FirstLL.java
@@ -0,0 +1,11 @@
+package linkedlist;
+
+public class FirstLL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is first LL");
+		
+	}
+
+}
