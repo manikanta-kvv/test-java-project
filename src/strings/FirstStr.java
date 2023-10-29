8ab787155502728447a012d58e0389f6688a7395
manikanta-kvv <106313314+manikanta-kvv@users.noreply.github.com>
2023-10-29 10:03:26 +0530

    String package added

---
+++ src/strings/FirstStr.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class FirstStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a string class");
+	}
+
+}
